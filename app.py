cd07ff8f39629e0b3e26eeb5b2e06505e8ea2677
Damian <damianlau96@gmail.com>
2025-03-01 14:13:07 +0800

    Update app to run on all interfaces by setting host to "0.0.0.0"

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def predict_car_prices():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(debug=False, port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)
